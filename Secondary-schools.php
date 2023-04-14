e718a3828ed6b167fdb7d45aafcae50adb93dc2d
Ifeanyi Nnaemego <josephifeanyid2@gmail.com>
2023-04-14 20:29:01 +0100

    secondary school list continue

--- Secondary-schools.php
+++ Secondary-schools.php
@@ -131,6 +131,265 @@
                         <td><a href="mailto:seatofwisdomcomprehsecschool@gmail.com">seatofwisdomcomprehsecschool@gmail.com</a></td>
                     </tr>
                     <tr>
+                        <td>S/C 0017</td>
+                        <td>Cor - Maria Secondary School, Nkpor</td>
+                        <td>Rev. Sr. Maria Onyinye Kristi Iregbe</td>
+                        <td>07032612715</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0018</td>
+                        <td>Sacred Heart Secondary School, Nteje</td>
+                        <td>Rev. Fr. Kizito Ezeniekwe</td>
+                        <td>07061101160</td>
+                        <td><a href="mailto:sacredheartcollegenteje@gmail.com">sacredheartcollegenteje@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0019</td>
+                        <td>Dominican Sister's Secondary School, Abatete</td>
+                        <td>Rev. Sr. Clementine Oti</td>
+                        <td>08036821251</td>
+                        <td><a href="mailto:dominicansisterscollegeabatete@gmail.com">dominicansisterscollegeabatete@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0020</td>
+                        <td>Our Lady's Secondary School, Umuoji Int'l</td>
+                        <td>Rev. Sr. Maureen J. Onwuanyia</td>
+                        <td>08147124502</td>
+                        <td><a href="mailto:ourladysacademy2020@yahoo.com">ourladysacademy2020@yahoo.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0021</td>
+                        <td>Madonna Secondary School, Nzam</td>
+                        <td>Bro. Amadi</td>
+                        <td>08063867065</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0022</td>
+                        <td>Marian Foundation Secondary School, Oraukwu</td>
+                        <td>Iloerika Chika O.</td>
+                        <td>08037585063</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0023</td>
+                        <td>Fr. Ohai Memorial Secondary School. Oroma - Etiti</td>
+                        <td>N/A</td>
+                        <td>08037618367 <br> 09033353742</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0024</td>
+                        <td>Michael Tansi Secondary School, Aguleri</td>
+                        <td>N/A</td>
+                        <td>N/A</td>
+                        <td><a href="mailto:michaeltansischoolaguleri@gmail.com">michaeltansischoolaguleri@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0025</td>
+                        <td>Mercy Girls' Secondary School, Umuoji</td>
+                        <td>Rev. Sr. Eustis M. Awiaka</td>
+                        <td>08037509093</td>
+                        <td><a href="mailto:mercygirlssecschoolumuoji@gmail.com">mercygirlssecschoolumuoji@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0026</td>
+                        <td>Holy Family Secondary School, Nkpor</td>
+                        <td>Nwankwo C.U</td>
+                        <td>08063845656</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0027</td>
+                        <td>ST. Patrick's Secondary School, Ugwuagba</td>
+                        <td>Rev. Sr. Daniel Genevieve</td>
+                        <td>08035129222</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0028</td>
+                        <td>Bethlehem Secondary School, Fegge Onitsha</td>
+                        <td>Ezeoke Genevieve</td>
+                        <td>08056328961</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0029</td>
+                        <td>ST. Raphael The Archangel Secondary School, Anumudu</td>
+                        <td>Mr. Anyadiem Emmanuel</td>
+                        <td>08063933807</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0030</td>
+                        <td>ST. Patrick's Secondary School, Okoti Odekpe</td>
+                        <td>Rev. Sr. Chinelo Omenyi</td>
+                        <td>08060262942</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0031</td>
+                        <td>Archbishop Val. Secondary School, Atani</td>
+                        <td>Ogbonna Chioma E.</td>
+                        <td>0808507401</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0032</td>
+                        <td>ST. Patrick Secondary School, Ossomala</td>
+                        <td>Emeka Nwadobu</td>
+                        <td>07060502888</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0033</td>
+                        <td>Mater Christi Secondary School, Umumbo</td>
+                        <td>N/A</td>
+                        <td>08032580486</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0034</td>
+                        <td>Sacred Heart Secondary School, ifite - Ogwari</td>
+                        <td>Nweke Christian</td>
+                        <td>09023886897</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0035</td>
+                        <td>ST. Joseph's Secondary School, Anaku</td>
+                        <td>Nwalieji E. Chikwado</td>
+                        <td>08064415440</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0036</td>
+                        <td>Regina Ceali Secondary School, Omor</td>
+                        <td>Anosike Jacinta</td>
+                        <td>07037260203</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0037</td>
+                        <td>ST. Paul's Secondary School, Umuleri</td>
+                        <td>Nworie Stephen Uchenna</td>
+                        <td>08064896384</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0038</td>
+                        <td>Madonna Secondary School, Umuleri</td>
+                        <td>Nworie Stephen uchenna</td>
+                        <td>08064896384</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0039</td>
+                        <td>SS. Peter & Paul Secondary School, Nkutaku</td>
+                        <td>Okoli Euphemie</td>
+                        <td>08064813002</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0040</td>
+                        <td>Mater Dei Secondary School, Alor</td>
+                        <td>Agbochiyo Grace</td>
+                        <td>08033598484</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0041</td>
+                        <td>Okigbo Memorial, Ojoto (ST. Odilias)</td>
+                        <td>Mrs. Mourah Ogechukwu</td>
+                        <td>08060314848</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0042</td>
+                        <td>ST. Raphael's Secondary School, Awkuzu</td>
+                        <td>Nwokoye Eugerie I.</td>
+                        <td>08035725376</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0043</td>
+                        <td>Holy Rosary Secondary School, Uke</td>
+                        <td>Rev. Sr. Nnenna Oriu</td>
+                        <td>08122048114</td>
+                        <td><a href="mailto:holyrosarysecsch@gmail.com">holyrosarysecsch@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0044</td>
+                        <td>Mater Dei Secondary School, Awkuzu</td>
+                        <td>Rev. Sr. Uzonna Orakwue</td>
+                        <td>08063789118</td>
+                        <td><a href="mailto:materdeisec@gmail.com">materdeisec@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0045</td>
+                        <td>ST. Charles Secondary School, Obosi Nkpor</td>
+                        <td>Ezebube Josephine</td>
+                        <td>08138682519</td>
+                        <td><a href="mailto:stcharlesborromeoschool@gmail.com">stcharlesborromeoschool@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0046</td>
+                        <td>Crux Sancta Secondary School, Nkpor</td>
+                        <td>Rev. Fr. Paschal Nchegha</td>
+                        <td>07061367153</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0047</td>
+                        <td>Albert De Greate College, Obosi</td>
+                        <td>Anyaeche Ifeoma</td>
+                        <td>08030614130</td>
+                        <td><a href="mailto:stablertdegreateschool@gmail.com">stablertdegreateschool@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0048</td>
+                        <td>Don Bosco Secondary School, Obosi</td>
+                        <td>Sir J.P.A Okeke</td>
+                        <td>08037423829</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0049</td>
+                        <td>John Odenigbo Memorial Secondary School, Omasi - Agu</td>
+                        <td>Rev. Sr. Godwin Okeji</td>
+                        <td>08064644431</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0050</td>
+                        <td>Mater Dei Secondary School, Aguleri</td>
+                        <td>Dan Nosike Chinedu</td>
+                        <td>07038502533</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0051</td>
+                        <td>ST. Patrick's Secondary School, Awkuzu</td>
+                        <td>N/A</td>
+                        <td>N/A</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0052</td>
+                        <td>ST. Patrick's Secondary School, Nkpikpa(New Haven)</td>
+                        <td>Ndu Chioma </td>
+                        <td>07038326732</td>
+                        <td><a href="mailto:chomycons36@gmail.com">chomycons36@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>S/C 0053</td>
+                        <td>ST. Anthony's Secondary School, Nteje</td>
+                        <td>Okeke Kate C.</td>
+                        <td>07064998930</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
                         <td></td>
                         <td></td>
                         <td></td>
