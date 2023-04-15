3d91571ad118e2d0851adc0b19eb8e74cfee1432
Ifeanyi Nnaemego <josephifeanyid2@gmail.com>
2023-04-15 11:17:21 +0100

    primary school list1 a

--- primary-schools.php
+++ primary-schools.php
@@ -24,25 +24,109 @@
                         <td>N/A</td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>N/P 0002</td>
+                        <td>Trinity N/P School, Onitsha</td>
+                        <td>Rev. Sr. Theresa Ilehukwu</td>
+                        <td>07030549956 <br> 07088086502</td>
+                        <td><a href="mailto:trinitybasilica@gmail.com">trinitybasilica@gmail.com</a></td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>N/P 0003</td>
+                        <td>Holy Child N/P School, Fegge</td>
+                        <td>Rev. Sr. Theresa Nnadi</td>
+                        <td>08033664924</td>
+                        <td>N/A</td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>N/P 0004</td>
+                        <td>ST. Anthony's N/P School, Umudioka</td>
+                        <td>Rev. Sr. Mary Lucia Odaghara</td>
+                        <td>0806619787</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0005</td>
+                        <td>Infant Jesus N/P School, Fegge</td>
+                        <td>Rev. Sr. Mmasichukwu Ideka</td>
+                        <td>07062961071</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0006</td>
+                        <td>Immaculate Conception N/P/S, Onitsha</td>
+                        <td>Rev. Sr. Maria Rose Ogudebe IHM</td>
+                        <td>0803969340</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0007</td>
+                        <td>Sacred Heart N/P/S, Odoakpu</td>
+                        <td>Rev. Sr. M. Daberechi Ekwuludu IHM</td>
+                        <td>08034756757</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0008</td>
+                        <td>Christ the King N/P/S, Onitsha</td>
+                        <td>Ifebighi Nkoli</td>
+                        <td>09031171225</td>
+                        <td><a href="mailto:ifebighinkoli@gmail.com">ifebighinkoli@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0009</td>
+                        <td>ST. charles Lwange N/P/S, Okpoko</td>
+                        <td>Mrs. Ezeukwu C. E. </td>
+                        <td>08038757062</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0010</td>
+                        <td>ST. Odilias N/P/S, Ojoto Uno</td>
+                        <td>Rev. Sr. Mary Laura Anyanou</td>
+                        <td>07067450271</td>
+                        <td><a href="mailto:stodilias34@gmail.com">stodilias34@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0011</td>
+                        <td>Ave Maria N/P School, Nnokwa</td>
+                        <td>Rev. Sr. Noelle Manie Ugwu</td>
+                        <td>08104180726</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0012</td>
+                        <td>Fr. Paul N/P/S, Awka - Etiti</td>
+                        <td>Ezenwanne Chikodili</td>
+                        <td>08167709928</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0013</td>
+                        <td>Holy Spirit N/P/S, Omagba</td>
+                        <td>Rev. Sr. Veronica Ukommadu M.</td>
+                        <td>08038715242</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0014</td>
+                        <td>Mater Christi N/P/S, Awada Obosi</td>
+                        <td>A. O. Ibeh Augustina</td>
+                        <td>07038728147</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0015</td>
+                        <td>SS Peter & Paul N/P/S, Nkutaku</td>
+                        <td>Mrs. Nwanna Helen E.</td>
+                        <td>08063213486</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>N/P 0016</td>
+                        <td>ST. Gregory N/P/S, Iyowa Odekpe</td>
+                        <td>rev. Sr. Miriam Florence Osuji</td>
+                        <td>0701282790</td>
+                        <td><a href="mailto:osujiflorence@gmail.com">osujiflorence@gmail.com</a></td>
                     </tr>
                     <tr>
                         <td></td>
